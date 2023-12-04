caa634a1962b3dce4ec9816e8471a489dd9b5095
Matevz <semprimoznik.matevz@gmail.com>
2023-12-04 14:57:25 +0100

    fix: auth header

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const Product = require('./productModel');
 const cors = require('cors');
-const axios = require("axios");
+const axios = require('axios');
 
 dotenv.config();
 
@@ -16,26 +16,26 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {dbName: 'db_products'});
+mongoose.connect(process.env.MONGODB_URI, { dbName: 'db_products' });
 
 app.use(express.json());
 
 // Swagger options
 const swaggerOptions = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'Products API',
-            version: '1.0.0',
-            description: 'API documentation for managing products.',
-        },
-        servers: [
-            {
-                url: `http://localhost:${port}`,
-            },
-        ],
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Products API',
+      version: '1.0.0',
+      description: 'API documentation for managing products.',
     },
-    apis: ['index.js'],
+    servers: [
+      {
+        url: `http://localhost:${port}`,
+      },
+    ],
+  },
+  apis: ['index.js'],
 };
 
 const swaggerSpec = swaggerJsdoc(swaggerOptions);
@@ -43,15 +43,17 @@ app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // Middleware to verify token
 const verifyToken = async (req, res, next) => {
-    try {
-        const response = await axios.get(`${process.env.AUTH_SERVICE_URL}/auth/verify`, { headers: {
-                'Authorization': req.headers.Authorization
-            }});
-        req.userId = response.data.userId
-    } catch (error) {
-        return res.status(401).json({ error: 'Unauthorized' });
-    }
-    next();
+  try {
+    const response = await axios.get(`${process.env.AUTH_SERVICE_URL}/auth/verify`, {
+      headers: {
+        Authorization: req.headers.authorization,
+      },
+    });
+    req.userId = response.data.userId;
+  } catch (error) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+  next();
 };
 
 // Create a new product
@@ -105,13 +107,13 @@ const verifyToken = async (req, res, next) => {
  *         description: Internal Server Error
  */
 app.post('/products', verifyToken, async (req, res) => {
-    try {
-        const product = new Product(req.body);
-        await product.save();
-        res.status(201).json(product);
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const product = new Product(req.body);
+    await product.save();
+    res.status(201).json(product);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Get all products
@@ -135,12 +137,12 @@ app.post('/products', verifyToken, async (req, res) => {
  *         description: Internal Server Error
  */
 app.get('/products', async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Search products by name
@@ -172,21 +174,21 @@ app.get('/products', async (req, res) => {
  *         description: Internal Server Error
  */
 app.get('/products/search', async (req, res) => {
-    try {
-        const { name } = req.query;
+  try {
+    const { name } = req.query;
 
-        if (!name) {
-            return res.status(400).json({ error: 'Missing search query parameter "name"' });
-        }
-
-        const regex = new RegExp(name, 'i'); // Case-insensitive search
-        const products = await Product.find({ name: { $regex: regex } });
-
-        res.status(200).json(products);
-    } catch (error) {
-        console.error(error); // Log the error for debugging purposes
-        res.status(500).json({ error: 'Internal Server Error' });
+    if (!name) {
+      return res.status(400).json({ error: 'Missing search query parameter "name"' });
     }
+
+    const regex = new RegExp(name, 'i'); // Case-insensitive search
+    const products = await Product.find({ name: { $regex: regex } });
+
+    res.status(200).json(products);
+  } catch (error) {
+    console.error(error); // Log the error for debugging purposes
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Get unique categories
@@ -210,26 +212,26 @@ app.get('/products/search', async (req, res) => {
  *         description: Internal Server Error
  */
 app.get('/products/categories', async (req, res) => {
-    try {
-        const categories = await Product.distinct('category');
-        res.status(200).json(categories);
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const categories = await Product.distinct('category');
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Get a single product by ID
 app.get('/products/:id', async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id);
-        if (!product) {
-            res.status(404).json({ error: 'Product not found' });
-        } else {
-            res.status(200).json(product);
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      res.status(404).json({ error: 'Product not found' });
+    } else {
+      res.status(200).json(product);
     }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Update a product by ID
@@ -260,18 +262,18 @@ app.get('/products/:id', async (req, res) => {
  *         description: Internal Server Error
  */
 app.put('/products/:id', verifyToken, async (req, res) => {
-    try {
-        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
-            new: true,
-        });
-        if (!product) {
-            res.status(404).json({ error: 'Product not found' });
-        } else {
-            res.status(200).json(product);
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!product) {
+      res.status(404).json({ error: 'Product not found' });
+    } else {
+      res.status(200).json(product);
     }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Delete a product by ID
@@ -298,16 +300,16 @@ app.put('/products/:id', verifyToken, async (req, res) => {
  *         description: Internal Server Error
  */
 app.delete('/products/:id', verifyToken, async (req, res) => {
-    try {
-        const product = await Product.findByIdAndDelete(req.params.id);
-        if (!product) {
-            res.status(404).json({ error: 'Product not found' });
-        } else {
-            res.status(204).end();
-        }
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) {
+      res.status(404).json({ error: 'Product not found' });
+    } else {
+      res.status(204).end();
     }
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Get products by category
@@ -338,12 +340,12 @@ app.delete('/products/:id', verifyToken, async (req, res) => {
  *         description: Internal Server Error
  */
 app.get('/products/category/:category', async (req, res) => {
-    try {
-        const products = await Product.find({ category: req.params.category });
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const products = await Product.find({ category: req.params.category });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Get all products sorted by a specific criteria (e.g., price)
@@ -376,18 +378,18 @@ app.get('/products/category/:category', async (req, res) => {
  *         description: Internal Server Error
  */
 app.get('/products/sorted/:criteria', async (req, res) => {
-    try {
-        const criteria = req.params.criteria.toLowerCase();
-        const validCriteria = ['originalprice', 'name', 'temporaryprice']; // Add more valid criteria as needed
-        if (!validCriteria.includes(criteria)) {
-            return res.status(400).json({ error: 'Invalid sorting criteria' });
-        }
-
-        const products = await Product.find().sort({ [criteria]: 1 });
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
+  try {
+    const criteria = req.params.criteria.toLowerCase();
+    const validCriteria = ['originalprice', 'name', 'temporaryprice']; // Add more valid criteria as needed
+    if (!validCriteria.includes(criteria)) {
+      return res.status(400).json({ error: 'Invalid sorting criteria' });
     }
+
+    const products = await Product.find().sort({ [criteria]: 1 });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Add discount to product
@@ -428,47 +430,47 @@ app.get('/products/sorted/:criteria', async (req, res) => {
  *         description: Internal Server Error.
  */
 app.put('/products/:productId/discount', verifyToken, async (req, res) => {
-    try {
-        const productId = req.params.productId;
-        const { temporaryPrice } = req.body;
+  try {
+    const productId = req.params.productId;
+    const { temporaryPrice } = req.body;
 
-        // Validate input
-        if (!productId || !temporaryPrice) {
-            return res.status(400).json({ error: 'Invalid data' });
-        }
-
-        // Find the product by ID
-        const product = await Product.findById(productId);
-
-        if (!product) {
-            return res.status(404).json({ error: 'Product not found' });
-        }
-
-        // Update the temporaryPrice
-        product.temporaryPrice = temporaryPrice;
-
-        // Save the updated product
-        await product.save();
-
-        res.status(200).json(product);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
+    // Validate input
+    if (!productId || !temporaryPrice) {
+      return res.status(400).json({ error: 'Invalid data' });
     }
+
+    // Find the product by ID
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ error: 'Product not found' });
+    }
+
+    // Update the temporaryPrice
+    product.temporaryPrice = temporaryPrice;
+
+    // Save the updated product
+    await product.save();
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 //todo get products with ids for cart
 app.get('/products/cart/:ids', async (req, res) => {
-    try {
-        const ids = req.params.ids.split(',');
-        const products = await Product.find({ _id: { $in: ids } });
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+  try {
+    const ids = req.params.ids.split(',');
+    const products = await Product.find({ _id: { $in: ids } });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
